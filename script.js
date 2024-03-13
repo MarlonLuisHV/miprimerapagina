3a02ab76560c73debd476024ce6e718d23760cfb
MarlonLuisHV <huaringavalverde16@gmail.com>
2024-03-12 19:47:18 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre1 = "Martín";
+//nombre1= "Carlos";
+
+//const nombre2 = "Martín";
+
+//var nombre3 = "Martín";
+
+//console.log(nombre1);
+//console.log(nombre2);
+//console.log(nombre3);
+
+//let publico = "CHIQUES"
+//let admin = "OTRO WEY"
+//let coso = "UNA MAQUETA DEL SISTEMA SOLAR"
+
+//let parrafo = document.querySelector(".parrafo2");
+
+//function cambiarTexto(publico, admin, coso) {
+    //let contenido `HOLA ${publico}, BIENVENIDOS A LA MARAVILLOSA PÁGINA WEB CREADA POR ${admin}, EN ESTE LUGAR ENCONTRARAS COSAS SIMPLES Y MUY COMUNES HECHAS POR MÍ CON EL PROPÓSITO DE APRENDER A PROGRAMAR UNA ${coso}.`;
+
+    //return contenido;
+//}
+
+//parrafo.innerText = cambiarTexto(publico, admin, coso);
